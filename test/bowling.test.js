66658b05bd52816da9a696353dd864d1fcf69f86
Yohann HERBET <yherbet@perig.com>
2023-10-03 13:40:38 +0200

    initial commit

---
+++ test/bowling.test.js
@@ -0,0 +1,11 @@
+const { assert }  = require('chai')
+const Bowling = require('../bowling.js')
+
+describe('Bowling', function() {
+    let bowling;
+    beforeEach(function() {
+        bowling = new Bowling();
+    });
+
+  it('should implement the bowling game rules');
+});
